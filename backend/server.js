9583bba10889855f0365f04c3d0df1c9854d4906
cangenesis-healthcare <info@cangenesis.com>
2024-11-22 13:00:24 +0530

    server issue changes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const fs = require('fs');
+const doctors = require("./doctors.json");
 const app = express();
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/images', express.static(path.join(__dirname, 'images'))); 
-
-app.use("/api/doctors", express.static(path.join(__dirname, "doctors.json")));
-app.use(express.static(path.join(__dirname, "../build")));
+// Serve doctors API
 app.get("/api/doctors", (req, res) => {
   const { specialization } = req.query;
   console.log('Received specialization:', specialization); // Debugging
@@ -29,16 +22,25 @@ app.get("/api/doctors", (req, res) => {
   const filteredDoctors = doctors.filter((doctor) => {
     const doctorSpecializations = doctor.specialization || []; // Default to empty array if undefined
     return doctorSpecializations.some((spec) =>
-      spec.toLowerCase().includes(specialization.toLowerCase())
+      spec.toLowerCase() === specialization.toLowerCase()
     );
   });
 
+  console.log("Filtered Doctors:", filteredDoctors); // Debugging
   res.json(filteredDoctors);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
